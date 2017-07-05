608df2459b667245f24920a3958d8dec1623ce50
Bigbrass <donrwalsh@gmail.com>
2017-07-05 16:35:40 -0500

    Taps and swipes up and running
    From the SlideShow view, you can tap, swipe down or swipe left for an
    action. Swiping up and right are available but unused. Tapping prints
    potato, and swiping left or down will navigate to the config page, or a
    new page for custom images respectively.

--- OpenHouseCheckIn/SlideshowController.swift
+++ OpenHouseCheckIn/SlideshowController.swift
@@ -19,6 +19,7 @@ class SlideshowController: UIViewController {
     var ss_images = [SS_Image]()
     var imageSource = [ImageSource]()
     
+    @IBOutlet weak var clickImage: UIImageView!
     @IBOutlet weak var slideshow: ImageSlideshow!
     
     override func viewDidLoad() {
@@ -28,12 +29,21 @@ class SlideshowController: UIViewController {
             ss_images += savedSS_Images
         }
         
+        let swipeDown = UISwipeGestureRecognizer(target: self, action: #selector(self.respondToSwipeGesture))
+        swipeDown.direction = UISwipeGestureRecognizerDirection.down
+        self.view.addGestureRecognizer(swipeDown)
+        
+        let swipeLeft = UISwipeGestureRecognizer(target: self, action: #selector(self.respondToSwipeGesture))
+        swipeDown.direction = UISwipeGestureRecognizerDirection.left
+        self.view.addGestureRecognizer(swipeLeft)
+        
         slideshow.backgroundColor = UIColor.white
         slideshow.slideshowInterval = 5.0
-        slideshow.pageControlPosition = PageControlPosition.underScrollView
+        slideshow.pageControlPosition = PageControlPosition.hidden
         slideshow.pageControl.currentPageIndicatorTintColor = UIColor.lightGray
         slideshow.pageControl.pageIndicatorTintColor = UIColor.black
         slideshow.contentScaleMode = UIViewContentMode.scaleToFill
+        slideshow.draggingEnabled = false
         
         // optional way to show activity indicator during image load (skipping the line will show no activity indicator)
         slideshow.activityIndicator = DefaultActivityIndicator()
@@ -55,10 +65,41 @@ class SlideshowController: UIViewController {
         return true
     }
     
+    func respondToSwipeGesture(gesture: UIGestureRecognizer) {
+        
+        if let swipeGesture = gesture as? UISwipeGestureRecognizer {
+            
+            switch swipeGesture.direction {
+            case UISwipeGestureRecognizerDirection.right:
+                print("Swiped right")
+            case UISwipeGestureRecognizerDirection.down:
+                print("Swiped down")
+                let storyBoard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
+                let newViewController = storyBoard.instantiateViewController(withIdentifier: "downSwipe") as! downSwipe
+                self.present(newViewController, animated: true, completion: nil)
+            case UISwipeGestureRecognizerDirection.left:
+                let storyBoard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
+                let newViewController = storyBoard.instantiateViewController(withIdentifier: "configMainNav") 
+                self.present(newViewController, animated: true, completion: nil)
+                print("Swiped left")
+            case UISwipeGestureRecognizerDirection.up:
+                print("Swiped up")
+            default:
+                break
+            }
+        }
+    }
+    
     func didTap() {
         let fullScreenController = slideshow.presentFullScreenController(from: self)
         // set the activity indicator for full screen controller (skipping the line will show no activity indicator)
         fullScreenController.slideshow.activityIndicator = DefaultActivityIndicator(style: .white, color: nil)
+    }
+    @IBAction func swipeDown(_ sender: UISwipeGestureRecognizer) {
+        print("down")
+    }
+    @IBAction func onTap(_ sender: UITapGestureRecognizer) {
+        print("potato")
     }
     
     private func loadSS_Images() -> [SS_Image]?  {
---
+++ OpenHouseCheckIn/downSwipe.swift
@@ -0,0 +1,26 @@
+//
+//  downSwipe.swift
+//  OpenHouseCheckIn
+//
+//  Created by Laura Farvour on 7/5/17.
+//  Copyright © 2017 Don Walsh. All rights reserved.
+//
+
+import UIKit
+import os.log
+
+
+
+
+class downSwipe: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+}
